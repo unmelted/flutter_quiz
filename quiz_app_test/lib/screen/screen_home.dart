5311324de6c7a7eb8b5fe4c5134634ae7843c0f9
eunkyung <unmelted@naver.com>
2021-02-06 20:09:07 +0900

    home screen of app is first complete

--- quiz_app_test/lib/screen/screen_home.dart
+++ quiz_app_test/lib/screen/screen_home.dart
@@ -16,7 +16,7 @@ class _HomeScreentState extends State<HomeScreen> {
       child: Scaffold(
         appBar: AppBar(
           title: Text("My Quiz App"),
-          backgroundColor: Colors.deepPurple,
+          backgroundColor: Colors.deepPurple[200],
           leading: Container(),
         ),
         body: Column(
@@ -26,9 +26,73 @@ class _HomeScreentState extends State<HomeScreen> {
             Center(
               child: Image.asset('images/quiz.jpeg', width: width * 0.8),
             ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.024),
+            ),
+            Text(
+              '플러터 퀴즈 앱',
+              style: TextStyle(
+                fontSize: width * 0.065,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '퀴즈를 풀기 전 안내사항입니다. \n꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.',
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+            _buildStep(width, '2. 문제를 잘 읽고 정답을 고른뒤 \n다음 문제 버튼을 눌러주세요.'),
+            _buildStep(width, '3. 만점을 향해 도전해보세요'),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            Container(
+              padding: EdgeInsets.only(bottom: width * 0.36),
+              child: Center(
+                child: ButtonTheme(
+                  minWidth: width * 0.8,
+                  height: height * 0.05,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: RaisedButton(
+                    child: Text(
+                      '지금 퀴즈 풀기',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.deepPurple,
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildStep(double width, String title) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(
+        width * 0.048,
+        width * 0.024,
+        width * 0.048,
+        width * 0.024,
+      ),
+      child:
+          Row(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+        Icon(
+          Icons.check_box,
+          size: width * 0.04,
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: width * 0.024),
+        ),
+        Text(title),
+      ]),
     );
   }
 }
