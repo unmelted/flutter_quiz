1ca6a047f1d0d2b39c5a04bd2a369807ed020036
eunkyung <unmelted@naver.com>
2021-02-09 22:39:10 +0900

    screen quiz add.. but need to debug

--- README.md
+++ README.md
@@ -19,3 +19,7 @@ VS code extensions: Dart / Flutter / Rainbow Brackets
 https://medium.com/@hj.veronica.shim/%ED%94%8C%EB%9F%AC%ED%84%B0-flutter-%EC%8B%9C%EC%9E%91%ED%95%98%EA%B8%B0-1-%EA%B0%9C%EB%B0%9C-%ED%99%98%EA%B2%BD-%EA%B5%AC%EC%B6%95-1131711dd651   
 * For android   
 https://asbear.tistory.com/180
+
+## Simple Useful command
+* open -a Simulator
+* flutter run
--- quiz_app_test/lib/screen/screen_home.dart
+++ quiz_app_test/lib/screen/screen_home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_test/model/model_quiz.dart';
+import 'package:quiz_app_test/screen/screen_quiz.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,6 +8,24 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreentState extends State<HomeScreen> {
+  List<Quiz> quizs = [
+    Quiz.fromMap({
+      'title': 'test',
+      'candidates': ['a', 'b', 'c', 'd'],
+      'answer': 0
+    }),
+    Quiz.fromMap({
+      'title': 'test',
+      'candidates': ['a', 'b', 'c', 'd'],
+      'answer': 0
+    }),
+    Quiz.fromMap({
+      'title': 'test',
+      'candidates': ['a', 'b', 'c', 'd'],
+      'answer': 0
+    }),
+  ];
+
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
@@ -63,7 +83,14 @@ class _HomeScreentState extends State<HomeScreen> {
                       style: TextStyle(color: Colors.white),
                     ),
                     color: Colors.deepPurple,
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => QuizScreen(
+                                    quizs: quizs,
+                                  )));
+                    },
                   ),
                 ),
               ),
